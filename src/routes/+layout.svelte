ff9085cc0c1d95e39499effcf9edddeb86e4cfc7
Gem <110815694+Ringlings@users.noreply.github.com>
2023-06-27 13:27:22 +1000

    eeeeee

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<svelte:head>
+	<style>
+		:root {
+			--background-base: white;
+            --background-primary: #2f2442;
+            --background-secondary: #d6b8ff;
+			--text-base: black;
+            --text-primary: white;
+		}
+        
+		@media (prefers-color-scheme: dark) {
+            :root {
+                --background-base: #181720;
+                --background-primary: #d6b8ff;
+                --background-secondary: #2f2442;
+				--text-base: white;
+                --text-primary: black;
+			}
+		}
+
+		body {
+			background-color: var(--background-base);
+			color: var(--text-base);
+
+			display: flex;
+			flex-direction: column;
+
+            font-family: Arial, "sans serif";
+            font-size: 1.2em;
+		}
+	</style>
+</svelte:head>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script>
-	let memberCount = 'at least 2';
+	import { onMount } from 'svelte';
 
-	async function load() {
+	let memberCount = '...';
+
+	onMount(async () => {
 		const response = await fetch('https://api.pluralkit.me/v2/systems/vgspl/members');
 		const result = await response.json();
 
-		memberCount = result.length
-	}
-
-	load()
+		memberCount = result.length;
+	});
 </script>
 
 <svelte:head>
@@ -17,7 +17,7 @@
 
 <div class="namedrop">
 	<h1>Ringlings</h1>
-	<p>Chaotic collective.</p>
+	<p>Chaotic collective</p>
 </div>
 
 <p>
@@ -25,25 +25,45 @@
 	do :catresort:
 </p>
 
-<p>
-	We're a system of <span
-		title="yes This Number Doesn't Load Immediately. no We're Not Gonna Fix It"
-	>
-		{memberCount}
-	</span> known collectively as Gem. We do random stuff sometimes. All times, actually.
+<p class="left-anchor">
+	We're a system of <a href="https://fronters.ringlings.systems/all">{memberCount}</a> known collectively
+	as Gem. We do random stuff sometimes. All times, actually. What kinda stuff? Welllllll...
 </p>
 
 <style>
 	.namedrop {
-		width: 100%;
+		padding: 10px;
+		border-radius: 10px;
+
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+		align-self: center;
+
+		background-color: var(--background-primary);
+		color: var(--text-primary);
 	}
-	.namedrop h1 {
-		margin-bottom: 0em;
+	.namedrop * {
+		margin: 0em;
+		max-width: fit-content;
+		background-color: unset;
 	}
-	.namedrop p {
-		margin-top: 0em;
+
+	p {
+		max-width: 75%;
+		background-color: var(--background-secondary);
+		padding: 5px;
+		border-radius: 10px;
+		margin-left: 5%;
+		margin-right: 5%;
+	}
+
+	.left-anchor {
+		align-self: flex-end;
+		text-align: end;
+	}
+
+	a {
+		color: mediumslateblue;
 	}
 </style>
